2d0238c83df332cbed3dff8066bdac86e5211835
Tom Arild Jakobsen <tajakobsen@gmail.com>
2025-11-24 08:43:53 +0100

    Expose DEFAULT_XP_SERVER constant (#7)

--- src/preview.ts
+++ src/preview.ts
@@ -1,6 +1,20 @@
-import type { Parameters } from "@storybook/server";
-
 export type { Preview } from "@storybook/server";
+
+declare const process: { env?: { RESOURCES_DIR?: string; APP_NAME?: string } } | undefined;
+
+export const DEFAULT_XP_SERVER_URL = "http://localhost:8080/webapp/no.item.storybook";
+
+export const DEFAULT_XP_SERVER = {
+  url: DEFAULT_XP_SERVER_URL,
+  params: {
+    xpResourcesDirPath: process?.env?.RESOURCES_DIR,
+    xpAppName: process?.env?.APP_NAME,
+    matchers: JSON.stringify({
+      zonedDateTime: /Date$|Time$/i.toString(),
+      region: /Region$/i.toString(),
+    }),
+  },
+};
 
 export function createPreviewServerParams(matchers: {
   localDate?: RegExp;
@@ -8,7 +22,7 @@ export function createPreviewServerParams(matchers: {
   localDateTime?: RegExp;
   number?: RegExp;
   region?: RegExp;
-}): Parameters {
+}): { matchers: string } {
   const preparedMatchers = Object.keys(matchers).reduce<Record<string, string>>((res, key) => {
     const matcher = matchers[key as keyof typeof matchers];
     if (matcher) {
